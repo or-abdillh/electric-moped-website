1459dd8d4cc62504ee11e864692bb63205515133
or-abdillh <hans.abdillh05@gmail.com>
2024-08-18 09:58:12 +0800

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "selector",
+    content: ["index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#E9EBEA",
+                secondary: "#CECFD1",
+                accent: "#141311",
+            },
+        },
+    },
+    plugins: [],
+};
